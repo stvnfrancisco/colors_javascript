75eb64b27fa03424318d592a952da37d396ad7ec
stvnfrancisco <svnfrancisco@yahoo.com>
2015-08-01 12:31:56 -0700

    a simple javascript app that allows the user to change the background color by clicking a color of choice button

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+
+  $("button#white").click(function() {
+    $("body").removeClass();
+    $("body").addClass("white-background");
+  })
+
+  $("button#blue").click(function() {
+    $("body").removeClass();
+    $("body").addClass("blue-background");
+  })
+});
---
+++ spec/specs.js
@@ -0,0 +1,5 @@
+describe('button#red', function() {
+  it("changes color depending on which button you select", function() {
+    expect(button#red(red)).to.equal(red-background);
+  });
+});
